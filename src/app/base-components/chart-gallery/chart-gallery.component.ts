fcd992bd11b8948e72f50c5f14b71201c1e8b33f
Bob Rinehart <rinebob111185@gmail.com>
2021-09-05 12:02:06 -0700

    Starting Chart Gallery component implementation

--- src/app/base-components/chart-gallery/chart-gallery.component.ts
+++ src/app/base-components/chart-gallery/chart-gallery.component.ts
@@ -2,7 +2,9 @@ import { Component, OnChanges, OnInit, ChangeDetectionStrategy, Input, SimpleCha
 
 import { BehaviorSubject, Observable } from 'rxjs';
 
-import { PickerTableData } from 'src/app/common/interfaces';
+import { GalleryViewOptions, NavBarSelection, PickerTableData } from 'src/app/common/interfaces';
+import { DEFAULT_PICKER_TABLE_DATUM } from 'src/app/common/constants';
+
 @Component({
   selector: 'exp-chart-gallery',
   templateUrl: './chart-gallery.component.html',
@@ -10,14 +12,30 @@ import { PickerTableData } from 'src/app/common/interfaces';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ChartGalleryComponent implements OnInit {
-  @Input() galleryData: any[] = [];
+  @Input() navSelections: NavBarSelection[] = [];
+  @Input() mainChart: PickerTableData = DEFAULT_PICKER_TABLE_DATUM;
+  @Input() galleryData: PickerTableData[] = [];
 
   readonly galleryDataBS = new BehaviorSubject<PickerTableData[]>([]);
   readonly galleryData$: Observable<PickerTableData[]> = this.galleryDataBS;
 
+  readonly mainChartBS = new BehaviorSubject<PickerTableData>(DEFAULT_PICKER_TABLE_DATUM);
+  readonly mainChart$: Observable<PickerTableData> = this.mainChartBS;
+
+  fullscreen = false;
+  gallery = true;
+  filmstrip = false;
+
   constructor() { }
 
   ngOnChanges(changes: SimpleChanges) {
+    if (changes['mainChart']) {
+      console.log('cG ngOC changes-mainChart: ', changes['mainChart']);
+      
+
+      const data: PickerTableData = (changes['mainChart']).currentValue;
+      this.mainChartBS.next(data);
+    }
     if (changes['galleryData']) {
       console.log('cG ngOC changes-galleryData: ', changes['galleryData']);
       
@@ -29,6 +47,50 @@ export class ChartGalleryComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  handleNavSelection(selection: string) {
+    console.log('bCV hNS selection: ', selection);
+    switch(selection) { 
+      case GalleryViewOptions.FULLSCREEN: { 
+        this.showFullscreen();
+        break; 
+     } 
+     case GalleryViewOptions.GALLERY: { 
+        this.showGallery();
+        break; 
+     } 
+     case GalleryViewOptions.FILMSTRIP: { 
+       this.showFilmstrip();
+       break; 
+     }
+     default: { 
+        console.log('bCV hNS default dude wtf you doin here???')
+        break; 
+     } 
+   } 
+  }
+
+  showFullscreen() {
+    console.log('bCV sFu show fullscreen called');
+    this.fullscreen = true;
+    this.gallery = false;
+    this.filmstrip = false;
+
+  }
+  showGallery() {
+    console.log('bCV sG show gallery called');
+    this.fullscreen = false;
+    this.gallery = true;
+    this.filmstrip = false;
+
+  }
+  showFilmstrip() {
+    console.log('bCV sFi show filmstrip called');
+    this.fullscreen = false;
+    this.gallery = false;
+    this.filmstrip = true;
+
   }
 
 }
--- src/app/base-components/slim-nav-bar/slim-nav-bar.component.ts
+++ src/app/base-components/slim-nav-bar/slim-nav-bar.component.ts
@@ -1,4 +1,28 @@
-import { Component, EventEmitter, OnInit, ChangeDetectionStrategy, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, ChangeDetectionStrategy, Output, Input, SimpleChanges } from '@angular/core';
+
+import {BehaviorSubject, Observable} from 'rxjs';
+
+import { GalleryViewOptions, NavBarSelection } from 'src/app/common/interfaces';
+
+
+// interface NavBarSelection {
+//   buttonText: string;
+//   destination: string;
+// }
+
+// enum GalleryViewOptions {
+//   FULLSCREEN = 'fullscreen',
+//   GALLERY = 'gallery',
+//   FILMSTRIP = 'filmstrip',
+
+// } 
+
+// const galleryNavSelections = [
+//   {buttonText: GalleryViewOptions.FULLSCREEN},
+//   {buttonText: GalleryViewOptions.GALLERY},
+//   {buttonText: GalleryViewOptions.FILMSTRIP},
+
+// ];
 
 
 @Component({
@@ -8,16 +32,34 @@ import { Component, EventEmitter, OnInit, ChangeDetectionStrategy, Output } from
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SlimNavBarComponent implements OnInit {
-  @Output() selectedComponent = new EventEmitter<string>();
+  @Input() selectionOptions: NavBarSelection[] = [];
+  @Output() selection = new EventEmitter<string>();
+
+  navBarSelectionsBS = new BehaviorSubject<NavBarSelection[]>([]);
+  navBarSelections$: Observable<NavBarSelection[]> = this.navBarSelectionsBS;
 
   constructor() { }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['selectionOptions']) {
+      console.log('pT ngOC changes-selectionOptions: ', changes['selectionOptions']);
+      const data: NavBarSelection[] = (changes['selectionOptions']).currentValue;
+      this.navBarSelectionsBS.next(data);
+      for (const datum of this.navBarSelectionsBS.value) {
+        // console.log('pT ngOI datum: ', datum);
+      }
+    }
+
+    
+
+  }
 
   ngOnInit(): void {
   }
 
-  handleClick(component: string) {
-    console.log('sNB hC selectedComponent: ', component);
-    this.selectedComponent.emit(component);
+  handleClick(selection: string) {
+    console.log('sNB hC selection: ', selection);
+    this.selection.emit(selection);
   }
 
 }
