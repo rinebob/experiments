46082e23995fe17958c5e58aa3e8fb4d6c1fd3c7
Bob Rinehart <rinebob111185@gmail.com>
2021-09-06 11:45:35 -0700

    Implemented fullscreen, gallery and filmstrip view switching in ChartGallery component

--- src/app/base-components/chart-gallery/chart-gallery.component.ts
+++ src/app/base-components/chart-gallery/chart-gallery.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnChanges, OnInit, ChangeDetectionStrategy, Input, SimpleChanges } from '@angular/core';
+import { AfterViewInit, Component, OnChanges, OnInit, ChangeDetectionStrategy, Input, SimpleChanges, ViewChild, ViewContainerRef, TemplateRef, ViewRef } from '@angular/core';
 
 import { BehaviorSubject, Observable } from 'rxjs';
 
@@ -11,7 +11,11 @@ import { DEFAULT_PICKER_TABLE_DATUM } from 'src/app/common/constants';
   styleUrls: ['./chart-gallery.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ChartGalleryComponent implements OnInit {
+export class ChartGalleryComponent implements AfterViewInit, OnInit {
+  @ViewChild('container', {read: ViewContainerRef}) viewContainer!: ViewContainerRef;
+  @ViewChild('fullscreen', {read: TemplateRef}) fullscreenTpl;
+  @ViewChild('gallery', {read: TemplateRef}) galleryTpl;
+  @ViewChild('filmstrip', {read: TemplateRef}) filmstripTpl;
   @Input() navSelections: NavBarSelection[] = [];
   @Input() mainChart: PickerTableData = DEFAULT_PICKER_TABLE_DATUM;
   @Input() galleryData: PickerTableData[] = [];
@@ -22,24 +26,24 @@ export class ChartGalleryComponent implements OnInit {
   readonly mainChartBS = new BehaviorSubject<PickerTableData>(DEFAULT_PICKER_TABLE_DATUM);
   readonly mainChart$: Observable<PickerTableData> = this.mainChartBS;
 
-  fullscreen = false;
-  gallery = true;
-  filmstrip = false;
+  fullTpl: ViewRef;
+  galTpl: ViewRef;
+  stripTpl: ViewRef;
 
-  constructor() { }
+  showFull = false;
+  showGal = false;
+  showStrip = false;
+
+  constructor(public viewContainerRef: ViewContainerRef) { }
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes['mainChart']) {
       console.log('cG ngOC changes-mainChart: ', changes['mainChart']);
-      
-
       const data: PickerTableData = (changes['mainChart']).currentValue;
       this.mainChartBS.next(data);
     }
     if (changes['galleryData']) {
       console.log('cG ngOC changes-galleryData: ', changes['galleryData']);
-      
-
       const data: PickerTableData[] = (changes['galleryData']).currentValue;
       this.galleryDataBS.next(data);
     }
@@ -47,6 +51,15 @@ export class ChartGalleryComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    console.log('cG ngAVI container ref: ', this.viewContainer);
+    console.log('cG ngAVI fullscreenTpl ref: ', this.fullscreenTpl);
+    this.fullTpl = this.fullscreenTpl.createEmbeddedView();
+    this.galTpl = this.galleryTpl.createEmbeddedView();
+    this.stripTpl = this.filmstripTpl.createEmbeddedView();
+
   }
 
   handleNavSelection(selection: string) {
@@ -73,23 +86,29 @@ export class ChartGalleryComponent implements OnInit {
 
   showFullscreen() {
     console.log('bCV sFu show fullscreen called');
-    this.fullscreen = true;
-    this.gallery = false;
-    this.filmstrip = false;
+    this.showFull = true;
+    this.showGal = false;
+    this.showStrip = false;
+    this.viewContainer.clear();
+    this.viewContainer.insert(this.fullTpl);
 
   }
   showGallery() {
     console.log('bCV sG show gallery called');
-    this.fullscreen = false;
-    this.gallery = true;
-    this.filmstrip = false;
+    this.showFull = false;
+    this.showGal = true;
+    this.showStrip = false;
+    this.viewContainer.clear();
+    this.viewContainer.insert(this.galTpl);
 
   }
   showFilmstrip() {
     console.log('bCV sFi show filmstrip called');
-    this.fullscreen = false;
-    this.gallery = false;
-    this.filmstrip = true;
+    this.showFull = false;
+    this.showGal = false;
+    this.showStrip = true;
+    this.viewContainer.clear();
+    this.viewContainer.insert(this.stripTpl);
 
   }
 
