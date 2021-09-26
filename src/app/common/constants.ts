7457f7c18210b50821393fab71d4c3ba85dbb544
Bob Rinehart <rinebob111185@gmail.com>
2021-09-26 16:29:55 -0700

    Added constants and interfaces supporting ConfigBuilder Expiration radio buttons

--- src/app/common/constants.ts
+++ src/app/common/constants.ts
@@ -1,4 +1,4 @@
-import { GalleryViewOptions } from "./interfaces";
+import { ExpirationSeries, ExpirationTimeDistance, ExpriationTimeDistanceName, GalleryViewOptions } from "./interfaces";
 import { OptionPosition } from "./option_interfaces";
 
 export const DEFAULT_PICKER_TABLE_DATUM = {symbol: 'TSLA', company: 'Tesla, Inc.', price: 50, implVolty: 20};
@@ -19,22 +19,6 @@ export const GalleryNavSelections = [
   
 ];
 
-
-
-let getMonthValues = [
-    [0, 'JAN'],
-    [1, 'FEB'],
-    [2, 'MAR'],
-    [3, 'APR'],
-    [4, 'MAY'],
-    [5, 'JUN'],
-    [6, 'JUL'],
-    [7, 'AUG'],
-    [8, 'SEP'],
-    [9, 'OCT'],
-    [10, 'NOV'],
-    [11, 'DEC'],
-];
 
 export const MONTHS_MAP = new Map([
     [0, 'JAN'],
@@ -61,6 +45,22 @@ export const DAYS_MAP = new Map([
     [6, 'SAT'],
 ]);
 
+export const DAYS_IN_A_MONTH_MAP = new Map([
+    [0, 31],
+    [1, 28],
+    [2, 31],
+    [3, 30],
+    [4, 31],
+    [5, 30],
+    [6, 31],
+    [7, 31],
+    [8, 30],
+    [9, 31],
+    [10, 30],
+    [11, 31],
+]);
+
+
 //   Default OptionPosition
 export const DEFAULT_OPTION_POSITION: OptionPosition = {
     title: '',
@@ -76,3 +76,81 @@ export const DEFAULT_HISTORICAL_ANALYSIS_DATE = {
     price: 600,
     implVolty: 50,
   };
+
+// List of option expiration months to expiration
+export const OPTION_EXPIRATION_TIME_DISTANCES: ExpirationTimeDistance[] = [
+{
+    expSeries: ExpirationSeries.WEEKLY,
+    expName: ExpriationTimeDistanceName.ONE_WEEK,
+    expDist: 1,
+    expDay: 5,
+},
+{
+    expSeries: ExpirationSeries.WEEKLY,
+    expName: ExpriationTimeDistanceName.TWO_WEEK,
+    expDist: 2,
+    expDay: 5,
+},
+{
+    expSeries: ExpirationSeries.MONTHLY,
+    expName: ExpriationTimeDistanceName.ONE_MONTH,
+    expDist: 1,
+    expDay: 4,
+},
+{
+    expSeries: ExpirationSeries.MONTHLY,
+    expName: ExpriationTimeDistanceName.TWO_MONTH,
+    expDist: 2,
+    expDay: 4,
+},
+{
+    expSeries: ExpirationSeries.MONTHLY,
+    expName: ExpriationTimeDistanceName.THREE_MONTH,
+    expDist: 3,
+    expDay: 4,
+},
+{
+    expSeries: ExpirationSeries.QUARTERLY,
+    expName: ExpriationTimeDistanceName.SIX_MONTH,
+    expDist: 6,
+    expDay: 4,
+},
+{
+    expSeries: ExpirationSeries.QUARTERLY,
+    expName: ExpriationTimeDistanceName.NINE_MONTH,
+    expDist: 9,
+    expDay: 4,
+},
+{
+    expSeries: ExpirationSeries.QUARTERLY,
+    expName: ExpriationTimeDistanceName.TWELVE_MONTH,
+    expDist: 12,
+    expDay: 4,
+},
+{
+    expSeries: ExpirationSeries.QUARTERLY,
+    expName: ExpriationTimeDistanceName.FIFTEEN_MONTH,
+    expDist: 15,
+    expDay: 4,
+},
+{
+    expSeries: ExpirationSeries.QUARTERLY,
+    expName: ExpriationTimeDistanceName.EIGHTEEN_MONTH,
+    expDist: 18,
+    expDay: 4,
+},
+{
+    expSeries: ExpirationSeries.QUARTERLY,
+    expName: ExpriationTimeDistanceName.TWENTY_FOUR_MONTH,
+    expDist: 24,
+    expDay: 4,
+},
+];
+
+// MAR, JUN, SEP, DEC
+export const QUARTERLY_EXPIRATION_MONTHS = [2, 5, 8, 11];
+
+export const STANDARD_DAYS_IN_A_MONTH = 30;
+export const DAYS_IN_A_WEEK = 7;
+export const TWO_CALENDAR_WEEKS = 14;
+
--- src/app/common/interfaces.ts
+++ src/app/common/interfaces.ts
@@ -60,3 +60,36 @@ export interface TimeParts {
     min: number;
     sec: number;
 }  
+
+export interface ExpirationTimeDistance {
+    expName: ExpriationTimeDistanceName;
+    expSeries: ExpirationSeries;
+    expDist: number;        // time units (wk, mo, qtr) to expriation
+    expDay: number;         // day of week 0(sun) to 6(sat)
+}
+
+export interface ExpirationDate {
+    expName: ExpriationTimeDistanceName;
+    expDate: Date;        // actual expriation date
+    expDay: number;         // day of week 0(sun) to 6(sat)
+}
+
+export enum ExpriationTimeDistanceName {
+    ONE_WEEK = 'One week',
+    TWO_WEEK = 'Two week',
+    ONE_MONTH = 'One month',
+    TWO_MONTH = 'Two month',
+    THREE_MONTH = 'Three month',
+    SIX_MONTH = 'Six month',
+    NINE_MONTH = 'Nine month',
+    TWELVE_MONTH = 'Twelve month',
+    FIFTEEN_MONTH = 'Fifteen month',
+    EIGHTEEN_MONTH = 'Eighteen month',
+    TWENTY_FOUR_MONTH = 'Twenty-four month',
+}
+
+export enum ExpirationSeries {
+    WEEKLY = 'Weekly',
+    MONTHLY = 'Monthly',
+    QUARTERLY = 'Quarterly',
+}
