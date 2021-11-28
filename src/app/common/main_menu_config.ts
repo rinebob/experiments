220994d9589af66f38a08e283ae9bba65e8ac206
Bob Rinehart <rinebob111185@gmail.com>
2021-11-28 10:10:28 -0800

    Refactored sidenav menu to use menu config objects

---
+++ src/app/common/main_menu_config.ts
@@ -0,0 +1,101 @@
+
+
+export const visVolTopLevelMenu = [
+    {
+        label: 'Dashboard',
+        route: '/dashboard',
+        triggerFor: '',
+    },
+    {
+        label: 'Portfolio',
+        route: '/portfolio',
+        triggerFor: 'visVolPortfolioMenu',
+    },
+    {
+        label: 'Opportunities',
+        route: '/opportunities',
+        triggerFor: 'visVolOpportunitiesMenu',
+    },
+    {
+        label: 'Analysis',
+        route: '/analysis',
+        triggerFor: 'visVolAnalysisMenu',
+    },
+    {
+        label: 'Account',
+        route: '/account',
+    },
+    {
+        label: 'Help',
+        route: '/help',
+    },
+];
+
+export const visVolPortfolioMenu = [
+    {
+        label: 'Dashboard',
+        route: 'portfolio/dashboard',
+    },
+    {
+        label: 'Money positions',
+        route: 'portfolio/money-positions',
+    },
+];
+export const visVolOpportunitiesMenu = [
+    {
+        label: 'Dashboard',
+        route: 'opportunities/dashboard',
+    },
+    {
+        label: 'Earnings',
+        route: 'opportunities/earnings',
+    },
+];
+export const visVolAnalysisMenu = [
+    {
+        label: 'Position builder',
+        route: 'analysis/position-builder',
+    },
+    {
+        label: 'Simple chart',
+        route: 'analysis/simple-chart',
+    },
+];
+
+
+export const experimentsMenuConfig = [
+    {
+        label: 'Angular',
+        route: '/angular',
+    },
+    {
+        label: 'D3',
+        route: '/d3',
+    },
+    {
+        label: 'Games',
+        route: '/games',
+    },
+    {
+        label: 'Base components',
+        route: '/base-components',
+    },
+    {
+        label: 'Simple chart',
+        route: '/simple-chart',
+    },
+    {
+        label: 'Position builder',
+        route: '/position-builder',
+    },
+    {
+        label: 'CSS Grid',
+        route: '/grid',
+    },
+    {
+        label: 'CDK Scrolling',
+        route: '/scroll',
+    },
+
+];
+    
