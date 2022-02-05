1469f317856d6d4a41b901b903f2296b8c33ea8a
Bob Rinehart <rinebob111185@gmail.com>
2022-02-05 15:08:10 -0800

    Added fields to PositionTile component

--- src/app/visvol/portfolio/portfolio.component.ts
+++ src/app/visvol/portfolio/portfolio.component.ts
@@ -1,7 +1,14 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+
+import { OptionPosition, OptionSpreadConfigBase } from '../../common/option_interfaces';
 import { PositionBuilderService } from '../../services/position-builder.service';
-import { moneynessMap, MoneynessUnit, OptionLegBase, OptionPosition, OptionSpreadConfigBase, OptionSymbolMetadata, OptionType } from '../../common/option_interfaces';
-import { Observable, of } from 'rxjs';
+import {EQUITY_DATA_MAP} from '../../common/constants';
+// import { nflxData } from 'src/assets/data/nflx_21-0917';
+// import { tslaData } from 'src/assets/data/tsla_21-0917';
+
+import * as configs from '../../common/option_configs';
+import { Option } from 'black-scholes-js';
 
 @Component({
   selector: 'exp-portfolio',
@@ -11,13 +18,45 @@ import { Observable, of } from 'rxjs';
 })
 export class PortfolioComponent implements OnInit {
 
-  // items = new Array(30);
+  SYMBOLS = ['NFLX', 'TSLA'];
 
-  // positions$:Observable<OptionPosition[]> = of();
+  private readonly configsList: OptionSpreadConfigBase[] = [
+    configs.ATM_LONG_STRADDLE,
+    configs.IRON_CONDOR,
+    configs.VERTICAL_CALL_DEBIT_SPREAD,
+    configs.VERTICAL_CALL_CREDIT_SPREAD,
+    configs.TWENTY_DELTA_SHORT_STRANGLE,
+    configs.VERTICAL_PUT_DEBIT_SPREAD,
+    configs.VERTICAL_PUT_CREDIT_SPREAD,
+  ];
+
+  positionsBS = new BehaviorSubject<OptionPosition[]>([]);
+  positions$: Observable<OptionPosition[]> = this.positionsBS;
 
   constructor(private readonly posnBuilderService: PositionBuilderService) { }
 
   ngOnInit(): void {
+    this.generatePositions();
+
   }
+
+  generatePositions() {
+    let dates;
+    const positionsList: OptionPosition[] = [];
+
+    for (const symbol of this.SYMBOLS) {
+      const data = EQUITY_DATA_MAP.get(symbol);
+      // dates = this.generateTradingDates(data);
+      dates = this.posnBuilderService.generateTradingDates(data);
+      const positions = this.posnBuilderService.generateOptionPositions(symbol, dates, this.configsList)
+
+      positionsList.push(...positions);
+
+    }
+
+    this.positionsBS.next(positionsList);
+    console.log('p gP positions[0]: ', positionsList[0]);
+  }
+
 
 }
