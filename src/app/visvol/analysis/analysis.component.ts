65317a377b407182c0b47254a2c9b62c2a22b09a
Bob Rinehart <rinebob111185@gmail.com>
2022-03-20 11:20:47 -0700

    Added Analysis ConfigBuilder form UI elements

--- src/app/visvol/analysis/analysis.component.ts
+++ src/app/visvol/analysis/analysis.component.ts
@@ -1,5 +1,9 @@
 import {ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
+import { ExpriationTimeDistanceLabel } from '../../common/option_interfaces';
+import { ALL_OPTION_CONFIGS_LIST} from '../../common/option_configs';
+import { FormControl, FormGroup } from '@angular/forms';
+
 @Component({
   selector: 'exp-analysis',
   templateUrl: './analysis.component.html',
@@ -8,9 +12,30 @@ import {ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 })
 export class AnalysisComponent implements OnInit {
 
+  readonly expirations = Object.values(ExpriationTimeDistanceLabel);
+  readonly configsList = ALL_OPTION_CONFIGS_LIST;
+
+  symbolControl = new FormControl('');
+  openDateControl = new FormControl('');
+  expirationControl = new FormControl('');
+  optionConfigControl = new FormControl('');
+  nameControl = new FormControl('');
+  configForm: FormGroup;
+  
+
   constructor() { }
 
   ngOnInit(): void {
+    console.log('a ngOI expirations: ', this.expirations);
+
+    this.configForm = new FormGroup({
+      'symbolControl' : this.symbolControl,
+      'openDateControl' : this.openDateControl,
+      'expirationControl' : this.expirationControl,
+      'optionConfigControl' : this.optionConfigControl,
+      'nameControl' : this.nameControl,
+    });
+
   }
 
 }
--- src/app/visvol/analysis/analysis.module.ts
+++ src/app/visvol/analysis/analysis.module.ts
@@ -1,5 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
 
 import { AnalysisRoutingModule } from './analysis-routing.module';
 import { AnalysisComponent } from './analysis.component';
@@ -13,6 +19,12 @@ import { AnalysisDashboardComponent } from './analysis-dashboard/analysis-dashbo
   ],
   imports: [
     CommonModule,
+    ReactiveFormsModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatNativeDateModule,
+    MatSelectModule,
     AnalysisRoutingModule
   ]
 })
